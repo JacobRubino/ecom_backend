79f1fece56a14ed5d386cca7c8ed31b3882f1cee
Jacob Rubino <jacobrubino@gmail.com>
2023-06-26 15:53:27 -0400

    category routes, fixed async

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,58 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [ Product ]
+    });
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve categories' });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const  loneCategory = await Category.findByPk(req.params.id, {
+      include: [ Product ]
+    });
+    res.status(200).json(loneCategory)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const makeCategory = await Category.create(req.body);
+    res.status(200).json(makeCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(updateCategory);
+  }catch (err) { res.status(500).json(err); }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(deleteCategory);
+  } catch (err) { res.status(500).json(err); }
 });
 
 module.exports = router;
